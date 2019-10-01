3fa24b94e28e65479336cfe08315940a512eb703
Alexander Amundson <alex.amundson987@gmail.com>
2019-10-01 17:55:04 -0400

    tic tac toe board

--- Udemy/1tictactoe/script.js
+++ Udemy/1tictactoe/script.js
@@ -1,11 +1,9 @@
 const table1 = document.getElementsByTagName("table")[0];
-
 const cells = document.querySelectorAll("td");
  
 cells.forEach(element => { 
     element.addEventListener("click", function(){
-        element.style.border = "6px solid yellow";
-        console.log("------");
+        element.style.border = "5px solid yellow";
     });
     
 });
